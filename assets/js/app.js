ddddad98d469f857eae0c712fd84b7d764c161ab
PedroSebastiaan <pizarropedrov@gmail.com>
2021-05-06 20:51:26 -0400

    fix: smooth scroll applied

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,5 +2,21 @@ $(document).ready(function () {
     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
     var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
         return new bootstrap.Tooltip(tooltipTriggerEl)
-    })
+    });
+    const links = document.querySelectorAll(".page-header ul a");
+
+    for (const link of links) {
+        link.addEventListener("click", clickHandler);
+    }
+
+    function clickHandler(e) {
+        e.preventDefault();
+        const href = this.getAttribute("href");
+        const offsetTop = document.querySelector(href).offsetTop;
+
+        scroll({
+            top: offsetTop,
+            behavior: "smooth"
+        });
+    }
 });
