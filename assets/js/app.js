5f5f522a7cb8b816ce51c1a51aa7b665a72e07df
PedroSebastiaan <pizarropedrov@gmail.com>
2021-05-08 23:16:51 -0400

    fix: fix minor issues with popovers and modals

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,5 @@
 $(window).ready(function () {
+    
     var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
     var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
         return new bootstrap.Popover(popoverTriggerEl)
